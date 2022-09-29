9122df6db1e2d052c429700850f4d6a486504dc6
Димитрија Рашљић <dimitrije.r@live.com>
2022-09-29 20:07:05 +0200

    Removed dzd.sh

--- .local/bin/dzd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-weatherreport="${XDG_DATA_HOME:-$HOME/.local/share}/weatherreport"
-
-showweather() { printf "%s" "$(sed '16q;d' "$weatherreport" |
-        grep -wo "[0-9]*%" | sort -rn | sed "s/^/☔/g;1q" | tr -d '\n')";
-}
-
-showweather
